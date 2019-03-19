ede9618726ac069c4e79e19f73bd375e42e76401
Josh Grebler <jgreb314@gmail.com>
2019-03-19 14:40:45 -0400

    Ingredients no longer print to the screen but are added to the recipe object's member variables

--- Recipe.java
+++ Recipe.java
@@ -13,7 +13,7 @@ import org.jsoup.select.*;
 public class Recipe
 {
     private String name;
-    private List ingredients = new ArrayList();
+    private ArrayList<String> ingredients = new ArrayList<String>();
     
 
     /**
@@ -23,16 +23,12 @@ public class Recipe
     {
         name = doc.title();
         
-        
-        
-        System.out.println(name);
-        
         Element pageContent = doc.selectFirst("div.slider-container").selectFirst("div.site-content").selectFirst("div#main-content").selectFirst("div.recipe-container-outer").selectFirst("section.ar_recipe_index");
         
         Elements ingredientLists = pageContent.selectFirst("section.recipe-ingredients").selectFirst("div#polaris-app").select("ul");
         for (Element i : ingredientLists){
             for (Element j : i.select("li")) {
-                System.out.println(j.select("label").first().attr("title"));
+                ingredients.add(j.select("label").first().attr("title"));
             }
         } 
     }
