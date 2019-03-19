e39b849e1301dcef5c7ba12dde8559bb49cc62cf
Josh Grebler <jgreb314@gmail.com>
2019-03-19 14:54:52 -0400

    Rearranged class to purely provide search functionality

--- Search.java
+++ Search.java
@@ -1,5 +1,5 @@
 /**
- * RecipeFetch main class for testing
+ * Provides search functionality for RecipeFetch
  *
  * @author Joshua Grebler
  * @version 3/6/19
@@ -8,13 +8,10 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.*;
 import java.io.IOException;
 
-public class RecipeFetch
-{
-    public static void main(String[] args) {
-        recipeSearch("nachos");
-    }
-    
-    public static void recipeSearch(String term) {
+public class Search
+{   
+	//Provides basic search function that returns a populated Recipe object
+    public static Recipe search(String term) {
         String url = String.format("https://www.allrecipes.com/search/results/?wt=%s",term);        
         try {
             Document doc = Jsoup.connect(url).get();
@@ -24,7 +21,7 @@ public class RecipeFetch
             image = first_result.select(".grid-card-image-container").first();
             link = image.select("a").first();
             String recipe_url = link.attr("abs:href");
-                Recipe r = new Recipe(Jsoup.connect(recipe_url).get());
+            return new Recipe(Jsoup.connect(recipe_url).get());
         }
         catch (IOException e) {
             System.out.println("error!");
