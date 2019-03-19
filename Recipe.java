32f190963f757f47cb68fd6e8278b84dc49e6e84
Josh Grebler <jgreb314@gmail.com>
2019-03-19 15:00:39 -0400

    Added display and accesor methods

--- Recipe.java
+++ Recipe.java
@@ -11,24 +11,41 @@ import org.jsoup.select.*;
 
 public class Recipe
 {
-    private String name;
-    private ArrayList<String> ingredients = new ArrayList<String>();
+    private String name; //The name of the dish
+    private ArrayList<String> ingredients = new ArrayList<String>(); //the list of ingredients in the dish
     
 
-    /**
-     * Constructor for objects of class Recipe
-     */
-    public Recipe(Document doc)
+	// Constructor for objects of class Recipe
+    public Recipe(Document doc) // Class constructor takes an ALREADY ACCESSED jsoup doc as its sole argument. Queries are not to be made through the constructor
     {
-        name = doc.title();
+        name = doc.title().replace(" - Allrecipes.com", ""); // assigns dish name and removes trailing website title
         
+		//First in the scraping process, we drill down to the level of the actual page content in which we are interested
         Element pageContent = doc.selectFirst("div.slider-container").selectFirst("div.site-content").selectFirst("div#main-content").selectFirst("div.recipe-container-outer").selectFirst("section.ar_recipe_index");
         
+		//The ingredients can be split among one or more tables in the "recipe-ingredients" section, so we iterate through all of them
         Elements ingredientLists = pageContent.selectFirst("section.recipe-ingredients").selectFirst("div#polaris-app").select("ul");
-        for (Element i : ingredientLists){
+ 
+ 
+		for (Element i : ingredientLists){
             for (Element j : i.select("li")) {
+				//Having found the ingredients, we add each to the recipe object's list
                 ingredients.add(j.select("label").first().attr("title"));
             }
         } 
     }
+	
+	public void printIngredients() {
+		for (i in ingredients) {
+			System.out.println(i);
+		}
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public Arraylist<String> getIngredients() {
+		return ingredients;
+	}
 }
